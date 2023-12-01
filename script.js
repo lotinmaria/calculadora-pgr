a95bb212f759435c3624b7f6e8d84a908695fcdd
Luizaescarban <luizaescarbandasil@gmail.com>
2023-12-01 16:51:41 -0300

    termino da calculadora

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
- const previousOperationText = document.querySelector("#previous-operation");
- const currentOperationText = document.querySelector("#current-operation");
- const buttons = document.querySelectorAll("#buttons-container button");
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
 
- class Calculator{
-   constructor(previousOperationText, currentOperationText){
+class Calculator {
+   constructor(previousOperationText, currentOperationText) {
       this.previousOperationText = previousOperationText;
       this.currentOperationText = currentOperationText;
       this.currentOperation = "";
    }
    //add digit to calculator screen
-   addDigit(digit){
+   addDigit(digit) {
       // check if current operation already has a dot
       if (digit === "." && this.currentOperationText.innerText.includes(".")) {
          return
@@ -20,11 +20,20 @@
 
    // Process all calculator operations
 
-   processOperation(operation){
-      
+   processOperation(operation) {
+      // Check if current is empty
+      if (this.currentOperationText.innerText === "" && operation !== "C") {
+         // Change operation
+         if (this.previousOperationText.innerText !== "") {
+            this.changeOperation(operation);
+         }
+         return;
+      }
+
+
       // Get current and previous values
       let operationsValue;
-      const previous = +this.previousOperationText.innerText;
+      const previous = +this.previousOperationText.innerText.split(" ")[0];
       const current = +this.currentOperationText.innerText;
 
       switch (operation) {
@@ -32,33 +41,96 @@
             operationsValue = previous + current;
             this.updateScreen(operationsValue, operation, current, previous);
             break;
-      
+         case "-":
+            operationsValue = previous - current;
+            this.updateScreen(operationsValue, operation, current, previous);
+            break;
+         case "/":
+            operationsValue = previous / current;
+            this.updateScreen(operationsValue, operation, current, previous);
+            break;
+         case "*":
+            operationsValue = previous * current;
+            this.updateScreen(operationsValue, operation, current, previous);
+            break;
+            case "DEL":
+            this.processDelOperation();
+            break;
+            case "CE":
+            this.processClearCurrentOperation();
+            break;
+            
+            case "=":
+            this.processEqualOperation();
+            break;
          default:
             return;
       }
 
    }
    // Change values of the calculator screen
-   updateScreen(operationsValue = null, 
-      operation = null, 
-      current = null, 
+   updateScreen(operationsValue = null,
+      operation = null,
+      current = null,
       previous = null
-      ){
+   ) {
 
-      console.log(operationsValue, operation, current, previous)
-      
-      this.currentOperationText.innerText += this.currentOperation;
+      if (operationsValue === null) {
+         this.currentOperationText.innerText += this.currentOperation
+      } else {
+         // Check if value is zero, if it is just add current value
+         if (previous === 0) {
+            operationsValue = current;
+         }
+         // Add current value to previous
+         this.previousOperationText.innerText = `${operationsValue} ${operation}`;
+         this.currentOperationText.innerText = "";
+      }
    }
- }
-const calc = new Calculator (previousOperationText, currentOperationText);
- buttons.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-         const value = e.target.innerText;
+   // Change math operation
+   changeOperation(operation){
+      
+      const mathOperation = ["*", "/", "+", "-"];
 
-        if (+value >= 0 || value === ".") {
+      if(!mathOperation.includes(operation)){
+         return
+      }
+
+      this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation;
+   }
+
+// Delet the last digit
+
+   processDelOperation(){
+      this.currentOperationText.innerText = this.currentOperationText.innerText.slice(0, -1);
+   }
+
+// Clear current operation
+   processClearCurrentOperation(){
+      this.currentOperationText.innerText =  "";
+   }
+
+   // Clear all operations
+   processClearOperation(){
+      this.currentOperationText.innerText =  "";
+      this.previousOperationText.innerText =  "";
+   }
+
+   // Process an operation
+   processEqualOperation(){
+      const operation = previousOperationText.innerText.split(" ")[1];
+      this.processOperation(operation);
+   }
+}
+const calc = new Calculator(previousOperationText, currentOperationText);
+buttons.forEach((btn) => {
+   btn.addEventListener("click", (e) => {
+      const value = e.target.innerText;
+
+      if (+value >= 0 || value === ".") {
          calc.addDigit(value);
-        } else {
+      } else {
          calc.processOperation(value);
-        }
-    });
- });
+      }
+   });
+});
