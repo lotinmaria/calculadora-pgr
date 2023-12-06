87716f2ae8218e47f8c3b3cca0b99cb330e9ad5e
Luizaescarban <luizaescarbandasil@gmail.com>
2023-12-06 07:50:30 -0300

    mudança de tudo

--- script.js
+++ script.js
@@ -1,145 +1,62 @@
-const previousOperationText = document.querySelector("#previous-operation");
-const currentOperationText = document.querySelector("#current-operation");
-const buttons = document.querySelectorAll("#buttons-container button");
+/*é colocado dentro dos parenteses do querySelector oq queremos selecionar,
+neste caso sendo o .result(ponto usado por se referir a uma classe), a mesma coisa com o .igual*/
+const resultado = document.querySelector(".result");
+const conta = document.getElementById("conta");
+const confirmar = document.querySelector(".igual");
 
-class Calculator {
-  constructor(previousOperationText, currentOperationText) {
-    this.previousOperationText = previousOperationText;
-    this.currentOperationText = currentOperationText;
-    this.currentOperation = "";
-  }
+/*O insert significa que eu quero inserir aluma coisa. */
+function insert(valor) {
+  conta.innerHTML += valor;
+}
 
-  // add digit to calculator screen
-  addDigit(digit) {
-    console.log(digit);
-    // Check if number already has a dot
-    if (digit === "." && this.currentOperationText.innerText.includes(".")) {
-      return;
-    }
+/*A função clean é usada para limpar tudo que tá dentro do <p> */
+function clean() {
+  resultado.innerHTML = " ";
+  conta.innerHTML = " ";
+}
 
-    this.currentOperation = digit;
-    this.updateScreen();
-  }
-
-  // process all calculator operations
-  processOperation(operation) {
-    // Check if current value is empty
-    if (this.currentOperationText.innerText === "" && operation !== "C") {
-      // Change operation
-      if (this.previousOperationText.innerText !== "") {
-        this.changeOperation(operation);
-      }
-      return;
-    }
-
-    // Get current and previous values
-    let operationValue;
-    let previous = +this.previousOperationText.innerText.split(" ")[0];
-    let current = +this.currentOperationText.innerText;
-
-    switch (operation) {
-      case "+":
-        operationValue = previous + current;
-        this.updateScreen(operationValue, operation, current, previous);
-        break;
-      case "-":
-        operationValue = previous - current;
-        this.updateScreen(operationValue, operation, current, previous);
-        break;
-      case "*":
-        operationValue = previous * current;
-        this.updateScreen(operationValue, operation, current, previous);
-        break;
-      case "/":
-        operationValue = previous / current;
-        this.updateScreen(operationValue, operation, current, previous);
-        break;
-      case "DEL":
-        this.processDelOperator();
-        break;
-      case "CE":
-        this.processClearCurrentOperator();
-        break;
-      case "C":
-        this.processClearOperator();
-        break;
-      case "=":
-        this.processEqualOperator();
-        break;
-      default:
-        return;
-    }
-  }
-
-  // Change values of calculator screen
-  updateScreen(
-    operationValue = null,
-    operation = null,
-    current = null,
-    previous = null
-  ) {
-    if (operationValue === null) {
-      // Append number to current value
-      this.currentOperationText.innerText += this.currentOperation;
-    } else {
-      // Check if value is zero, if is just add current value
-      if (previous === 0) {
-        operationValue = current;
-      }
-      // Add current value to previous
-      this.previousOperationText.innerText = `${operationValue} ${operation}`;
-      this.currentOperationText.innerText = "";
-    }
-  }
-
-  // Change math operation
-  changeOperation(operation) {
-    const mathOperations = ["*", "-", "+", "/"];
-
-    if (!mathOperations.includes(operation)) {
-      return;
-    }
-
-    this.previousOperationText.innerText =
-      this.previousOperationText.innerText.slice(0, -1) + operation;
-  }
-
-  // Delete a digit
-  processDelOperator() {
-    this.currentOperationText.innerText =
-      this.currentOperationText.innerText.slice(0, -1);
-  }
-
-  // Clear current operation
-  processClearCurrentOperator() {
-    this.currentOperationText.innerText = "";
-  }
-
-  // Clear all operations
-  processClearOperator() {
-    this.currentOperationText.innerText = "";
-    this.previousOperationText.innerText = "";
-  }
-
-  // Process an operation
-  processEqualOperator() {
-    let operation = this.previousOperationText.innerText.split(" ")[1];
-
-    this.processOperation(operation);
+/*backspace é para qunado eu quero apagar somente o ultimo digito */
+function backspace() {
+  if (conta.textContent) {
+    let result = document.getElementById("conta").innerHTML;
+    conta.innerHTML = result.substring(0, result.length - 1);
   }
 }
 
-const calc = new Calculator(previousOperationText, currentOperationText);
+/*confirma é para enviar o resultado final ao p quando clicar no = */
+function confirma() {
+  if (resultado.textContent != "Erro") {
+    document.getElementById("resultado").innerHTML = eval(conta.innerHTML);
+  }
+}
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const value = e.target.innerText;
+/*verifica se uma tecla é apertada*/
+document.addEventListener("keydown", (event) => {
+  const keyName = event.key;
 
-    if (+value >= 0 || value === ".") {
-      console.log(value);
-      calc.addDigit(value);
-    } else {
-      calc.processOperation(value);
-    }
-  });
+  if (!isNaN(keyName)) {
+    insert(keyName);
+  }
+
+  if (
+    keyName == "/" ||
+    keyName == "*" ||
+    keyName == "+" ||
+    keyName == "-" ||
+    keyName == "."
+  ) {
+    insert(keyName);
+  }
+
+  if (keyName == "Enter") {
+    confirma();
+  }
+
+  if (keyName == "Delete") {
+    clean();
+  }
+
+  if (keyName == "Backspace") {
+    backspace();
+  }
 });
